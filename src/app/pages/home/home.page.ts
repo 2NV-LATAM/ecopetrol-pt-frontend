1746253427440de677302f9df3b3cde452336bcb
Arnold Ramírez Arévalo <arnold@3dementes-iMac.local>
2019-09-04 04:45:20 -0500

    First Screen

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,13 +3,17 @@ import {
   GoogleMap,
   GoogleMapsEvent,
   GoogleMapOptions,
+  GoogleMapsAnimation,
   CameraPosition,
   MarkerOptions,
   Marker,
+  ILatLng,
   Environment
 } from '@ionic-native/google-maps';
 import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderOptions, NativeGeocoderResult } from '@ionic-native/native-geocoder/ngx';
 
 @Component({
   selector: 'app-home',
@@ -17,44 +21,136 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+  city = "Bogotá";
+  temperature = "12º";
   map: GoogleMap;
 
-  constructor(private platform: Platform) { }
+  constructor(private platform: Platform, public geolocation: Geolocation, private nativeGeocoder: NativeGeocoder) { }
 
   loadMap() {
-
-    // This code is necessary for browser
     /*Environment.setEnv({
-      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyBMnlnVvmbxzX4JRfJrbhwYpp1MaNCycxA',
-      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyBMnlnVvmbxzX4JRfJrbhwYpp1MaNCycxA'
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyBL-rm6LKs3C4i_1ATXFtDQS6iM7Zyrc2g',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyBL-rm6LKs3C4i_1ATXFtDQS6iM7Zyrc2g'
     });*/
-
-    let mapOptions: GoogleMapOptions = {
-      camera: {
-        target: {
-          lat: 43.0741904,
-          lng: -89.3809802
+    this.geolocation.getCurrentPosition().then((position) => {
+      let initialPos: ILatLng = { lat: position.coords.latitude, lng: position.coords.longitude };
+      this.map = GoogleMaps.create('map_canvas', {
+        camera: {
+          target: {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          },
+          zoom: 18,
+          tilt: 30
         },
-        zoom: 18,
-        tilt: 30
-      }
+        styles: [
+          { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+          { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+          { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+          {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#d59563' }]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#d59563' }]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{ color: '#263c3f' }]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#6b9a76' }]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{ color: '#38414e' }]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{ color: '#212a37' }]
+          },
+          {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#9ca5b3' }]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{ color: '#746855' }]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{ color: '#1f2835' }]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#f3d19c' }]
+          },
+          {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{ color: '#2f3948' }]
+          },
+          {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#d59563' }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{ color: '#17263c' }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#515c6d' }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{ color: '#17263c' }]
+          }
+        ]
+      });
+
+      let marker: Marker = this.map.addMarkerSync({
+        title: 'My Ubicación',
+        position: initialPos,
+        animation: GoogleMapsAnimation.BOUNCE
+      });
+
+      this.getAddressFromCoords(position.coords.latitude, position.coords.longitude);
+
+    });
+  }
+
+  getAddressFromCoords(lattitude, longitude) {
+    console.log("getAddressFromCoords " + lattitude + " " + longitude);
+    let options: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 1
     };
 
-    this.map = GoogleMaps.create('map_canvas', mapOptions);
+    this.nativeGeocoder.reverseGeocode(lattitude, longitude, options)
+      .then((result: NativeGeocoderResult[]) => {
+        this.city = result[0].locality;
+      })
+      .catch((error: any) => {
+        this.city = "No disponible";
+      });
 
-    let marker: Marker = this.map.addMarkerSync({
-      title: 'Ionic',
-      icon: 'blue',
-      animation: 'DROP',
-      position: {
-        lat: 43.0741904,
-        lng: -89.3809802
-      }
-    });
-    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-      alert('clicked');
-    });
   }
 
   async ngOnInit() {
