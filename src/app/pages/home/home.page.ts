3e60f5e88ea349050c6e5799b76b65f2aadca22e
Arnold Ramírez Arévalo <arnold@3dementes-iMac.local>
2019-09-04 00:10:33 -0500

    First Commit

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,68 @@
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  map: GoogleMap;
+
+  constructor(private platform: Platform) { }
+
+  loadMap() {
+
+    // This code is necessary for browser
+    /*Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyBMnlnVvmbxzX4JRfJrbhwYpp1MaNCycxA',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyBMnlnVvmbxzX4JRfJrbhwYpp1MaNCycxA'
+    });*/
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
+
+  async ngOnInit() {
+    // Since ngOnInit() is executed before `deviceready` event,
+    // you have to wait the event.
+    await this.platform.ready();
+    await this.loadMap();
+  }
+
+
+}
